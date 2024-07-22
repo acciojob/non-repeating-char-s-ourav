a8e61faa3095c45fd3127f66a1ecb9729b595bbd
Sourav Mazumdar <sourav.mozu@gmail.com>
2024-07-22 22:20:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	arr=[]
+	for(i=0;i<str.length;i++){
+		ch=str.charAt(i)
+		cnt=0
+		for(j=0;j<str.length;j++){
+			if(str.charAt(j)==ch)
+				cnt++
+		}
+		if(cnt==1)
+			return ch
+	}
+	
+	return null
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
